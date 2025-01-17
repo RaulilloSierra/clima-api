2f091f5623667dac7d17a39cb056da97279666dd
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-01-17 17:19:34 -0500

    Segundo llamado a la API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -6,8 +6,11 @@ export default function useWeather() {
     const { VITE_API_KEY } = import.meta.env;
     try {
       const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${VITE_API_KEY}`;
-      const response = await axios.get(geoURL);
-      console.log(response.data);
+      const { data } = await axios.get(geoURL);
+      const { lat, lon } = data[0];
+      const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${VITE_API_KEY}`;
+      const { data: wheaterResult } = await axios.get(weatherURL);
+      console.log(wheaterResult);
     } catch (error) {
       console.log(error);
     }
