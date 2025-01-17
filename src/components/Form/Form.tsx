0b0c30ad24f979039f633d7af0d0140b30a663e5
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-01-17 11:49:43 -0500

    Estado local y validación de formulario

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,61 @@
+import { useState, ChangeEvent, FormEvent } from "react";
 import { countries } from "../../data/countries.ts";
+import type { SearchType } from "../../types/index.ts";
+import Alert from "../Alert/Alert.tsx";
 import style from "./Form.module.css";
 
 function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: "",
+    country: "",
+  });
+
+  const [alert, setAlert] = useState("");
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (Object.values(search).includes("")) {
+      setAlert("Todos los campos son obligatorios");
+      return;
+    }
+  };
+
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={handleSubmit}>
+      {alert && (
+        <p>
+          <Alert>{alert}</Alert>
+        </p>
+      )}
       <div className={style.field}>
         <label htmlFor="city">Ciudad</label>
-        <input id="city" type="text" name="city" placeholder="Ciudad" />
+        <input
+          id="city"
+          type="text"
+          name="city"
+          value={search.city}
+          placeholder="Ciudad"
+          onChange={handleChange}
+        />
       </div>
       <div className={style.field}>
         <label htmlFor="country">País</label>
-        <select name="" id="">
+        <select
+          name="country"
+          id="country"
+          value={search.country}
+          onChange={handleChange}
+        >
           <option value="">Seleccione...</option>
           {countries.map((country) => (
             <option key={country.code} value={country.code}>
