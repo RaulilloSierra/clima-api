5a9d0375eaef3ae2313580e041c6a5d8e65154c2
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-01-17 10:48:26 -0500

    estilos en app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import style from "./App.module.css";
+
 function App() {
   return (
     <>
-      <h1>Buscador de clima</h1>
+      <h1 className={style.title}>Buscador de clima</h1>
+      <div className={style.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   );
 }
