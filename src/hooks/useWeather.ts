b26294f64311dcff33aed821326cf257306a09f8
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-01-19 11:07:17 -0500

    Aplicacion de Zod

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,17 +1,33 @@
 import axios from "axios";
-import { SearchType, WeatherType } from "../types";
+import { z } from "zod";
+import { SearchType } from "../types";
 
-function isWeatherResult(wheater: unknown): wheater is WeatherType {
-  return (
-    Boolean(wheater) &&
-    typeof wheater === "object" &&
-    typeof (wheater as WeatherType).name === "string" &&
-    typeof (wheater as WeatherType).main.temp === "number" &&
-    typeof (wheater as WeatherType).main.temp_min === "number" &&
-    typeof (wheater as WeatherType).main.temp_max === "number" &&
-    typeof (wheater as WeatherType).main.humidity === "number"
-  );
-}
+// Type Guard o assertion
+// function isWeatherResult(wheater: unknown): wheater is WeatherType {
+//   return (
+//     Boolean(wheater) &&
+//     typeof wheater === "object" &&
+//     typeof (wheater as WeatherType).name === "string" &&
+//     typeof (wheater as WeatherType).main.temp === "number" &&
+//     typeof (wheater as WeatherType).main.temp_min === "number" &&
+//     typeof (wheater as WeatherType).main.temp_max === "number" &&
+//     typeof (wheater as WeatherType).main.humidity === "number"
+//   );
+// }
+
+// Zod
+
+const WeatherSchema = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_min: z.number(),
+    temp_max: z.number(),
+    humidity: z.number(),
+  }),
+});
+
+type Weather = z.infer<typeof WeatherSchema>;
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -22,12 +38,22 @@ export default function useWeather() {
       const { lat, lon } = data[0];
       const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${VITE_API_KEY}`;
       // Type Guard
+      // const { data: wheaterResult } = await axios(weatherURL);
+      // console.log(wheaterResult);
+      // const result = isWeatherResult(wheaterResult);
+      // if (result) {
+      //   console.log(wheaterResult.name);
+      // } else {
+      //   console.log("No se encontraron resultados");
+      // }
+
+      // Implementacion de Zod
       const { data: wheaterResult } = await axios(weatherURL);
-      console.log(wheaterResult);
-      const result = isWeatherResult(wheaterResult);
-      if (result) {
-        console.log(wheaterResult.name);
-      } else {
+      const result = WeatherSchema.safeParse(wheaterResult);
+      if (result.success) {
+        console.log(result.data.name);
+        console.log(result.data.main.temp);
+      }else{
         console.log("No se encontraron resultados");
       }
     } catch (error) {
