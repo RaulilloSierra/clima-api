1c9c57ddd4cecc2ed908044ca4ad7eece28f4c81
RaulilloSierra <raulitosierradiaz@gmail.com>
2025-01-18 19:19:22 -0500

    Ajustes menores

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
-import { SearchType } from "../types";
+import { SearchType, WeatherType } from "../types";
+
+function isWeatherResult(wheater: unknown): wheater is WeatherType {
+  return (
+    Boolean(wheater) &&
+    typeof wheater === "object" &&
+    typeof (wheater as WeatherType).name === "string" &&
+    typeof (wheater as WeatherType).main.temp === "number" &&
+    typeof (wheater as WeatherType).main.temp_min === "number" &&
+    typeof (wheater as WeatherType).main.temp_max === "number" &&
+    typeof (wheater as WeatherType).main.humidity === "number"
+  );
+}
 
 export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
@@ -9,8 +21,15 @@ export default function useWeather() {
       const { data } = await axios.get(geoURL);
       const { lat, lon } = data[0];
       const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${VITE_API_KEY}`;
-      const { data: wheaterResult } = await axios.get(weatherURL);
+      // Type Guard
+      const { data: wheaterResult } = await axios(weatherURL);
       console.log(wheaterResult);
+      const result = isWeatherResult(wheaterResult);
+      if (result) {
+        console.log(wheaterResult.name);
+      } else {
+        console.log("No se encontraron resultados");
+      }
     } catch (error) {
       console.log(error);
     }
